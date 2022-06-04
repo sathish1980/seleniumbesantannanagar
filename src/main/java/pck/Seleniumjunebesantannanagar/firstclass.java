c12d0ae36daf431ec2e0f47a5839eee5911f5858
sathish1980 <kumar.sathish189@gmail.com>
2022-06-04 09:03:26 +0530

    created a new selenium and maven project

---
+++ src/main/java/pck/Seleniumjunebesantannanagar/firstclass.java
@@ -0,0 +1,16 @@
+package pck.Seleniumjunebesantannanagar;
+
+public class firstclass {
+	
+	public void add()
+	{
+		System.out.println("this is addition");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		firstclass f = new firstclass();
+		f.add();
+	}
+
+}
